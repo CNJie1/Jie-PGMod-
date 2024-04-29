061ccf87f0eff8d9060adbdd67cf27fb7688b639
print <110807969+CNJie1@users.noreply.github.com>
2024-04-30 05:20:32 +0800

    Update vue1.js

--- vue1.js
+++ vue1.js
@@ -1,7 +1,9 @@
 // 偏移量
-const offsets = {
-    offset1: 0x2B9BF48,//等级
-    offset2: 0x2B9BF48,
-    offset3: 0xABCDEF,
-    // 添加更多的偏移量...
+var offsets = {
+    offset1: "0x2B9BF48",
+    offset2: "0x2B9BF48",
+    // 可以添加更多的偏移量...
 };
+
+// 导出 offsets 对象，以便其他 JavaScript 文件可以访问
+module.exports = offsets;
